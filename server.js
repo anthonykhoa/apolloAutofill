fd88a97cba439c105b72088d392abc4b0e95f19b
root <root@hwsrv-756779.hostwindsdns.com>
2022-08-10 00:07:42 +0000

    add

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schema');
-const express = require('express');
-const app = express();
+const { ApolloServer } = require("apollo-server-express")
+const { typeDefs, resolvers } = require("./schema")
+const express = require("express")
+const app = express()
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-});
+})
 
-app.use('/public/dist', (req, res) => res.sendFile(__dirname + '/public/dist/bundle.js'));
-app.get('/apolloPOC', (req, res) => res.sendFile(__dirname + '/views/apolloPOC.html'));
-app.get('/reactPOC', (req, res) => res.sendFile(__dirname + '/views/reactPOC.html'));
+app.use("/public/dist", (req, res) =>
+  res.sendFile(__dirname + "/public/dist/bundle.js")
+)
+app.get("/apolloPOC", (req, res) =>
+  res.sendFile(__dirname + "/views/apolloPOC.html")
+)
+app.get("/reactPOC", (req, res) =>
+  res.sendFile(__dirname + "/views/reactPOC.html")
+)
+;(async function startServer() {
+  await server.start()
+  server.applyMiddleware({ app })
 
-(async function startServer() {
-	await server.start();
-	server.applyMiddleware({ app });
-
-	app.listen({ port: 4000 }, () =>
-		console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-	);
-}())
+  app.listen({ port: 3015 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  )
+})()
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from "react"
+import ReactDOM from "react-dom"
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   gql,
   useMutation,
-} from '@apollo/client';
+} from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: "https://autofill.khoa.app/graphql",
   cache: new InMemoryCache(),
-});
+})
 
 const POTATO_AUTH_MUTATION = gql`
   mutation PotatoAuth {
@@ -19,12 +19,12 @@ const POTATO_AUTH_MUTATION = gql`
       isPotato
     }
   }
-`;
+`
 
 function App() {
-  const [pubCredVerifiedState, setPubCredVerifiedState] = useState(false);
-  const [pubCredState, setPubCredState] = useState('');
-  const [passwordState, setPasswordState] = useState('');
+  const [pubCredVerifiedState, setPubCredVerifiedState] = useState(false)
+  const [pubCredState, setPubCredState] = useState("")
+  const [passwordState, setPasswordState] = useState("")
   return (
     <ApolloProvider client={client}>
       <form autoComplete="on" noValidate>
@@ -46,7 +46,7 @@ function App() {
         )}
       </form>
     </ApolloProvider>
-  );
+  )
 }
 function PasswordView({
   pubCredState,
@@ -57,15 +57,15 @@ function PasswordView({
 }) {
   const [potatoChangeRequest] = useMutation(POTATO_AUTH_MUTATION, {
     onCompleted: async (data) => {
-      setPubCredVerifiedState(false);
-      setPubCredState('');
+      setPubCredVerifiedState(false)
+      setPubCredState("")
     },
-  });
+  })
   const [potatoLogin] = useMutation(POTATO_AUTH_MUTATION, {
     onCompleted: async (data) => {
-      window.location.assign('https://www.google.com');
+      window.location.assign("https://www.google.com")
     },
-  });
+  })
   return (
     <>
       <input
@@ -75,7 +75,7 @@ function PasswordView({
         onChange={(e) => setPasswordState(e.target.value)}
       />
       <input
-        style={{ opacity: 0, zIndex: -99, position: 'absolute' }}
+        style={{ opacity: 0, zIndex: -99, position: "absolute" }}
         type="email"
         value={pubCredState}
         autoComplete="username"
@@ -83,8 +83,8 @@ function PasswordView({
       <button
         type="submit"
         onClick={(e) => {
-          e.preventDefault();
-          potatoLogin();
+          e.preventDefault()
+          potatoLogin()
         }}
       >
         login
@@ -92,14 +92,14 @@ function PasswordView({
       <button
         type="text"
         onClick={(e) => {
-          e.preventDefault();
-          potatoChangeRequest();
+          e.preventDefault()
+          potatoChangeRequest()
         }}
       >
         change
       </button>
     </>
-  );
+  )
 }
 function PubCredView({
   setPubCredVerifiedState,
@@ -109,34 +109,45 @@ function PubCredView({
 }) {
   const [potatoAuthRequest] = useMutation(POTATO_AUTH_MUTATION, {
     onCompleted: async (data) => {
-      setPubCredVerifiedState(true);
+      setPubCredVerifiedState(true)
     },
-  });
+  })
   return (
     <>
+      {false && (
+        <input
+          type="email"
+          autoComplete="username"
+          value={pubCredState}
+          onChange={(e) => setPubCredState(e.target.value)}
+        />
+      )}
       <input
-        type="email"
-        autoComplete="username"
-        value={pubCredState}
-        onChange={(e) => setPubCredState(e.target.value)}
+        class="ppvx_text-input__control___3-13-16"
+        name="email"
+        id="text-input-email"
+        aria-invalid="false"
+        placeholder=" "
       />
-      <input
-        style={{ opacity: 0, zIndex: -99, position: 'absolute' }}
-        autoComplete="current-password"
-        type="password"
-      />
+      <label
+        for="text-input-email"
+        id="text-input-email-label"
+        class="ppvx_text-input__label___3-13-16"
+      >
+        Email or phone number
+      </label>
       <button
         id="next"
         type="submit"
         onClick={(e) => {
-          e.preventDefault();
-          potatoAuthRequest();
+          e.preventDefault()
+          potatoAuthRequest()
         }}
       >
         next
       </button>
     </>
-  );
+  )
 }
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector("#app"))
